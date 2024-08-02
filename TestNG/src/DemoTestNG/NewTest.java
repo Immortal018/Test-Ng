392f6684b8577d3364691a958a3f523b06c0233f
Immortal018 <110182013+Immortal018@users.noreply.github.com>
2024-08-02 13:03:44 +0530

    code update NewTest.java

--- TestNG/src/DemoTestNG/NewTest.java
+++ TestNG/src/DemoTestNG/NewTest.java
@@ -9,7 +9,7 @@ public class NewTest {
 	public void demo() {
 		
 		for (int i = 1; i <=10 ; i++) {
-			System.out.println("Hello world");
+			System.out.println("Hello world vignesh");
 		}
 		
 	}
